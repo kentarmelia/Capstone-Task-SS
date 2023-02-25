f4537351e92d8ee72543586a738ad79bbcdf7280
kent armelia <70646421+kentarmelia@users.noreply.github.com>
2023-02-25 21:47:00 +0800

    adding loader pages

--- TaskSS/src/app/app-routing.module.ts
+++ TaskSS/src/app/app-routing.module.ts
@@ -10,7 +10,15 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'loader',
+    loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule)
   }
+
 ];
 
 @NgModule({
